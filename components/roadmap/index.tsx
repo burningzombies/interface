4d88819d154f684975516b177ff5df663d257cc8
Talha Akkaya <talha2akkaya@gmail.com>
2021-12-02 04:35:26 -0500

    roadmap text updated

--- components/roadmap/index.tsx
+++ components/roadmap/index.tsx
@@ -2,9 +2,38 @@ import React from "react";
 import roadmapIMG from "../../assets/roadmap.png";
 import Image from "next/image";
 
-export const Roadmap: React.FC = () => {
+const renderTitle = () => {
   return (
     <div>
+      <div className="d-sm-block d-xs-block d-md-none d-lg-none">
+        <h2 className="hero-text fw-bold">
+          Roadmap{" "}
+          <i
+            className="ms-1 fas fa-exclamation"
+            style={{ transform: "rotate(15deg)" }}
+          ></i>
+        </h2>
+      </div>
+      <div className="d-none d-lg-block d-md-block">
+        <h2 className="hero-text fw-bold">
+          How is it going?{" "}
+          <i
+            className="ms-1 fas fa-exclamation"
+            style={{ transform: "rotate(15deg)" }}
+          ></i>
+        </h2>
+        <p className="lead fw-bold text-light text-shadow">
+          Here is the known roadmap for Burning Zombies.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export const Roadmap: React.FC = () => {
+  return (
+    <div className="text-center">
+      {renderTitle()}
       <div className="row">
         <Image
           src={roadmapIMG}
