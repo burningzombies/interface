383a34140ff9d7ec873b9249cb0008cdac6c2578
Talha Akkaya <talha2akkaya@gmail.com>
2021-12-05 09:17:31 -0500

    segment no added

--- components/mint/info/index.tsx
+++ components/mint/info/index.tsx
@@ -14,6 +14,7 @@ export const Info: React.FC<Props> = ({ address, masterContract }) => {
   const [maxTokenPerWallet, setMaxTokenPerWallet] = React.useState<
     number | undefined
   >();
+  const [segmentNo, setSegmentNo] = React.useState<number | undefined>();
 
   React.useEffect(() => {
     let isMounted = true;
@@ -27,13 +28,20 @@ export const Info: React.FC<Props> = ({ address, masterContract }) => {
           await masterContract.MAX_TOKEN_PER_WALLET()
         ).toNumber();
 
+        const segmentSize = (await masterContract.segmentSize()).toNumber();
+        const totalSupply = (await masterContract.totalSupply()).toNumber();
+        const segmentNo_ = (totalSupply / segmentSize).toString();
+        const segmentNo = parseInt(segmentNo_);
+
         if (isMounted) {
           setMaxTokenPerTx(maxTokenPerTx);
           setMaxTokenPerWallet(maxTokenPerWallet);
+          setSegmentNo(segmentNo);
         }
       } catch {
         setMaxTokenPerTx(undefined);
         setMaxTokenPerWallet(undefined);
+        setSegmentNo(undefined);
       }
     };
     init();
@@ -42,29 +50,64 @@ export const Info: React.FC<Props> = ({ address, masterContract }) => {
     };
   }, [masterContract]);
 
+  const renderMaxTokenPerTx = () => {
+    return maxTokenPerTx ? (
+      <strong>{maxTokenPerTx}</strong>
+    ) : (
+      <Spinner color="text-light" />
+    );
+  };
+
+  const renderMaxTokenPerWallet = () => {
+    return maxTokenPerWallet ? (
+      <strong>
+        <Balance {...{ masterContract, owner: address }} />/{maxTokenPerWallet}
+      </strong>
+    ) : (
+      <Spinner color="text-light" />
+    );
+  };
+
+  const renderSegment = () => {
+    const renderBenefits = (segmentNo: number): string => {
+      switch (segmentNo) {
+        case 0: {
+          return "90% Discount for $NEMO minters.";
+        }
+        case 1: {
+          return "50% Discount for $NEMO minters.";
+        }
+        default: {
+          return "TBA";
+        }
+      }
+    };
+    return segmentNo ? (
+      <>
+        <i className="fas fa-info-circle me-2"></i>
+        <strong className="me-2">Segment {segmentNo}</strong>(
+        {renderBenefits(segmentNo)})
+      </>
+    ) : (
+      <Spinner color="text-light" />
+    );
+  };
+
   return (
-    <>
-      <small className="d-block">
-        * Maximum{" "}
-        <strong>
-          {maxTokenPerTx ? maxTokenPerTx : <Spinner color="text-light" />}
-        </strong>{" "}
-        zombies can mint, once.
-      </small>
-      <small className="d-block">
-        ** Maximum{" "}
-        <strong>
-          {maxTokenPerWallet ? (
-            <>
-              <Balance {...{ masterContract, owner: address }} />/
-              {maxTokenPerWallet}
-            </>
-          ) : (
-            <Spinner color="text-light" />
-          )}
-        </strong>{" "}
-        zombies can collect per wallet.
-      </small>
-    </>
+    <ul className="list-unstyled mb-0">
+      <li>
+        <span className="small">{renderSegment()}</span>
+      </li>
+      <li>
+        <span className="small">
+          * Maximum {renderMaxTokenPerTx()} zombies can mint, once.
+        </span>
+      </li>
+      <li>
+        <span className="small">
+          ** Maximum {renderMaxTokenPerWallet()} zombies can collect per wallet.
+        </span>
+      </li>
+    </ul>
   );
 };
