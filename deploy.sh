4b62c6c7475cdf44761a57c71559b59372a488ff
Talha Akkaya <talha2akkaya@gmail.com>
2022-01-26 10:36:28 -0500

    26140

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ]; then
+	MSG="$RANDOM"
+else
+	MSG="$1"
+fi
+
+echo "Prettier"
+npm run prettier &&
+
+echo "Linter"
+npm run lint &&
+
+echo "Compiler"
+npx tsc &&
+
+echo "Git"
+git add -A &&
+git commit -m $MSG &&
+git push -u origin main
+
