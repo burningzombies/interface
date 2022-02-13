d46c5eb67abe2764ce8e926f171c77cbfe10bd8c
Talha Akkaya <talha2akkaya@gmail.com>
2022-02-13 10:15:21 -0500

    20887

--- utils/index.ts
+++ utils/index.ts
@@ -63,7 +63,7 @@ export const parseTier = (
 
 export const fetcher = <TData>(query: string): Promise<TData> => {
   return request(
-    "https://api.thegraph.com/subgraphs/id/QmaHwSgWcyYQsHPchwEBNmVsxBWMUeaAgHw1PUDecuRqJy",
+    "https://api.thegraph.com/subgraphs/id/QmZXcWcDdvxDZzLogRuYZhSMkz7LguMQuRyD6CKFPXTFqc",
     query
   );
 };
